b2f46c881d575013e454a95d9c805f42f131219d
drumond-king <nathaniel091004@gmail.com>
2024-12-15 23:23:53 +0800

    Itemkeranjang

---
+++ ItemKeranjang.java
@@ -0,0 +1,24 @@
+class ItemKeranjang {
+    private ItemKeranjang head;
+    private ItemKeranjang tail;
+    private int size;
+
+    public ItemKeranjang() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    class KeranjangBelanja {
+        private ItemKeranjang head;
+        private ItemKeranjang tail;
+        private int size;
+    
+        public KeranjangBelanja() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+    
+   
+
+   
--- tes.java
+++
@@ -1,9 +0,0 @@
-class tes {
-    String merek;
-    int harga;
-
-    public tes(String merek, int harga){
-        this.merek = merek;
-        this.harga = harga;
-    }
-}
