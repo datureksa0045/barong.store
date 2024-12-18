0965f15e6399b728cc08d2471739b4aa59f926bb
B4R0N9 <danuartawiraguna@gmail.com>
2024-12-18 23:54:30 +0800

    Add files via upload

--- NodeProduk.java
+++ NodeProduk.java
@@ -1,3 +1,19 @@
 public class NodeProduk {
-    
+    String id;
+    String nama;
+    double harga;
+    String kategori;
+    String ukuran;
+    String warna;
+    NodeProduk nextProduk; 
+
+    public NodeProduk(String id, String nama, double harga, String kategori, String ukuran, String warna) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+        this.kategori = kategori;
+        this.ukuran = ukuran;
+        this.warna = warna;
+        this.nextProduk = null;
+    }
 }
--- Produk.java
+++ Produk.java
@@ -1,3 +1,11 @@
-public class Produk {
+public class Produk{
+    NodeProduk produk;
+    Produk next;
+    Produk prev;
 
+    public Produk(NodeProduk produk) {
+        this.produk = produk;
+        this.next = null;
+        this.prev = null;
+    }
 }
